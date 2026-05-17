54d6c49a71b8d847255927699d524b0d3609d868
Abhishek.hit <abhishek539700@gmail.com>
2026-05-17 13:15:03 +0530

    solve more questions

--- Pattern/Q20.java
+++ Pattern/Q20.java
@@ -1,13 +1,13 @@
 package Pattern;
 
-public class Q12 {
+public class Q20 {
     public static void main(String[] args) {
         int n=7;
         for (int i=1;i<=n;i++){
-            for (int j=1;j<n-i+1;j++){
-                System.out.print (" ");
+            for (int j=1;j<i;j++){
+                System.out.print ("  ");
             }
-            for (int j=1;j<=i;j++){
+            for (int j=1;j<n-i;j++){
                 System.out.print (j+" " );
             }
             System.out.println ( );
--- Pattern/Q8.java
+++ Pattern/Q8.java
@@ -5,7 +5,7 @@ public class Q8 {
         int n=7;
         for (int i=n;i>=1;i--){
             for (int j=i;j>=1;j--){
-                System.out.print ( " "+j);
+                System.out.print ( "  "+j);
             }
             System.out.println ( );
         }
