266f2b4c7fe80117d8c19a47e632f3dce856ee3d
Abhishek.hit <abhishek539700@gmail.com>
2026-05-19 22:38:40 +0530

    solve more questions

--- Java100Plus/CountchAndDuplicatech.java
+++ Java100Plus/CountchAndDuplicatech.java
@@ -26,7 +26,7 @@ public class CountchAndDuplicatech {
 //         System.out.println(chars[i] + " ki count: " +count);//total chr kitni bar aur
 //            ye count
             if (count>1){
-                System.out.println( "Duplicate "+chars[i]+" count-> "+count );
+                System.out.println( "Duplicate "+chars[i]+" counts-> "+count );
             }
         }
     }
