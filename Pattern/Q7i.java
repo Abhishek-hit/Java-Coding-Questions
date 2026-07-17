fa4aef50a83d3157a13475c578ff84d1cceb6187
Abhishek Sharma <abhishek539700@gmail.com>
2026-07-17 23:13:38 +0530

    Update Q7i.java

--- Pattern/Q7i.java
+++ Pattern/Q7i.java
@@ -5,7 +5,7 @@ public class Q7i {
         int n=7;
         for (int i=1;i<=n;i++){
             for (int j=i;j>=1;j--){
-                System.out.print (" *"+ j );
+                System.out.print ("  *"+ j );
             }
             System.out.println ( );
         }
