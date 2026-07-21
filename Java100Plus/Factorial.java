81d7805ebe9a5d3f62f2c65512c2fa82b4c475ac
Abhishek Sharma <abhishek539700@gmail.com>
2026-07-21 23:17:11 +0530

    Update Factorial.java

--- Java100Plus/Factorial.java
+++ Java100Plus/Factorial.java
@@ -3,7 +3,7 @@ package Java100Plus;
 public class Factorial {
     public static void main(String[] args) {
         int f=1;
-        int num=5;
+        int num=6;
         for (int i=1;i<=num;i++){
             f=f*i;
         }  System.out.print (f );
