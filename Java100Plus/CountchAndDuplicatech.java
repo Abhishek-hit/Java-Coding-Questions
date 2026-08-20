7470590704ede8160e7cb6382a29e4600b290502
Abhishek.hit <abhishek539700@gmail.com>
2026-08-20 12:17:11 +0530

    java revision

--- Java100Plus/CountchAndDuplicatech.java
+++ Java100Plus/CountchAndDuplicatech.java
@@ -6,28 +6,28 @@ public class CountchAndDuplicatech {
 //        char[] chars=str.toCharArray ();
         String[] chars=str.split (" ");
 
-       boolean[] visited=new boolean[chars.length];
+        boolean[] visited=new boolean[chars.length];
         for (int i=0;i<chars.length;i++){
             if (visited[i]){
                 continue;
             }
-            String currentChar=chars[i];
+            // String currentChar=chars[i];
             int count=1;
 
             for (int j=i+1; j<chars.length; j++){
 
-              //  if (currentChar == chars[j]) for char ke liye
-                if (currentChar.equals(chars[j])){
+                //  if (currentChar == chars[j]) for char ke liye
+                if (chars[i].equals(chars[j])){
                     count++;
                     visited[j] = true; //duplicate milta h use visited kro
                 }
 
             }
-//         System.out.println(chars[i] + " ki count: " +count);//total chr kitni bar aur
+            System.out.println(chars[i] + " ki count: " +count);//total chr kitni bar aur
 //            ye count
-            if (count>1){
-                System.out.println( "Duplicate "+chars[i]+" counts-> "+count );
-            }
+//            if (count>1){
+//                System.out.println( "Duplicate "+chars[i]+" counts-> "+count );
+//            }
         }
     }
 }
